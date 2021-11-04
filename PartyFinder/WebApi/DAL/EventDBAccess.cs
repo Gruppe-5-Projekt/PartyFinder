a41daa552dc9e53f3e20385ba3de8e300721cbea
VigtorWolf <73114751+VigtorWolf@users.noreply.github.com>
2021-11-04 13:33:14 +0100

    MiniUpdate

---
+++ PartyFinder/WebApi/DAL/EventDBAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.DAL
+{
+    public class EventDBAccess : IEventDBAccess
+    {
+        /*public List<Event> GetAllEvents()
+        {
+            
+        }*/
+    }
+}
